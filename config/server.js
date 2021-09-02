94233785c9121b198defc35fb34e2498d5bfd656
Claudio Barsante <clbmribas@gmail.com>
2021-09-02 15:27:55 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb81ea5b8ec194ea54d85c77784b04da'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dff0fe76-df01-445c-8163-01c7fd70fcc5'
+};
